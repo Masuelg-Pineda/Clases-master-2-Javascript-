4fae024b3b44d7bf1099d8791951bdb52fc20883
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-03-12 19:08:55 -0600

    Se agregan ejemplos para visualizar

--- Clase12MAR26.js
+++ Clase12MAR26.js
@@ -6,6 +6,9 @@ const productos = [
     { nombre: "Libro", precio: 12, categoria: "Educación" },
     { nombre: "Zapatos", precio: 50, categoria: "Ropa" },
     { nombre: "Celular", precio: 600, categoria: "Electrónica" },
+    { nombre: "Comida para perro", precio: 1100, categoria: "Mascotas" }, //Se agrega ejemplo
+    { nombre: "Alpina 300cc", precio: 50000, categoria: "Electrónica" }, // Se agrega ejemplo para visualizar
+    { nombre: "Picafresa", precio: 2, categoria: "Confitería" }, // Se agrega ejemplo para visualizar
 ];
 
 //Encadenamiento de metodos
@@ -13,7 +16,7 @@ const resultado = productos
 //filter
 .filter(producto => producto.precio < 100) //Productos que cuesten menos de 100
 //Sort
-.sort((a, b) => a.nombre.localeCompare(b.nombre)) //LocalCompare compara string 
+.sort((a, b) => a.nombre.localeCompare(b.nombre)) //LocalCompare compara strings correctamente y ordenamos alfabeticamente
 //map
 .map(producto => producto.nombre); //Devuelve solo el nombre del producto
 
