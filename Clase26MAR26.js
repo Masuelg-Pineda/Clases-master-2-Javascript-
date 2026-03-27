55bc02841ccbc2adea5209d72a1905c5e7107b4f
Masuelg-Pineda <jostin.masuelg.pineda@gmail.com>
2026-03-26 19:54:01 -0600

    Avance de divide y venderas

--- Clase26MAR26.js
+++ Clase26MAR26.js
@@ -1,27 +1,34 @@
 // "Busqueda binaria"
 
 function binarySearch(arr, target, low = 0, high = arr.length - 1){
+    
+    console.log(`Buscando el numero ${target} entre los indices ${low} y ${high}`)
     //Caso base, donde ya no podamos buscar
     if (low > high){
-        return -1;
+        console.log("No encontrado")
+        return -1; //No lo encontro
     }
 
     //Paso 1. Dividir
     let mid = Math.floor((low + high) / 2);
+    console.log(`MID: ${mid}, Valor: ${arr[mid]}`)
 
     //Paso 2. Conquistar
     if(arr[mid] === target){
+        console.log(`Encontrado en el indice ${mid}`)
         return mid; //Si encontramos ese valor
     }
 
     //Paso 3. Seguir dividiendo
     if(arr[mid] > target){
         //Buscamos en la mitad izquierda
+        console.log("Buscamos en la mitad izquierda")
         return binarySearch(arr, target, low, mid - 1);
     }else {
         //Buscar en la mitad derecha
+        console.log("Buscamos en la mitad derecha")
         return binarySearch(arr, target, low, mid + 1, high);
     }
 }
 
-console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 5));
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 9));
